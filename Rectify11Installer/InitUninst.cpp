631fac08f829c67b116abad5fc1e6c9b26889cb6
RetroFrost <routerifter@gmail.com>
2026-08-21 23:04:48 +0100

    Harden uninstaller DirectUI initialisation

--- Rectify11Installer/InitUninst.cpp
+++ Rectify11Installer/InitUninst.cpp
@@ -10,8 +10,7 @@ RichText* useless;
 
 int InitUninstPages() {
     for (int i = 1; i < MAXUNINSTPAGE; i++) {
-
-        Element* page;
+        Element* page = nullptr;
         MainLogger.WriteLine(L"Setting XML parser from resource: " + std::to_wstring(IDR_UIFILE8 + i));
         err2 = pParser->SetXMLFromResource(IDR_UIFILE8 + i, hinst, hinst);
         if (FAILED(err2)) {
@@ -30,14 +29,16 @@ int InitUninstPages() {
 
         MainLogger.WriteLine(L"Creating PageMain Element inside " + n);
         err2 = pParser->CreateElement((UCString)L"PageMain", pContainer, NULL, NULL, &page);
-        if (FAILED(err2)) {
+        if (FAILED(err2) || !page) {
+            if (SUCCEEDED(err2)) err2 = -33;
             MainLogger.WriteLine(L"Cannot create PageMain Element inside " + n, err2);
+            return err2;
         }
 
         std::wstring anim = L"animator" + std::to_wstring(i);
         MainLogger.WriteLine(L"Finding Element with ID: " + anim);
         Element* pAnimator = pMain->FindDescendent(StrToID((UCString)anim.c_str()));
-        if (FAILED(err2)) {
+        if (!pAnimator) {
             err2 = -31;
             MainLogger.WriteLine(L"Element with ID: " + anim + L" not found", err2);
             return err2;
@@ -54,44 +55,48 @@ int InitUninstPages() {
         MainLogger.WriteLine(L"Adding page " + std::to_wstring(i) + L" to page list");
         animArr.push_back(pAnimator);
         pageArr.push_back(pContainer);
-
         MainLogger.WriteLine(L"\n");
     }
     MainLogger.WriteLine(L"InitUninstPages() completed", err2);
     return err2;
 }
 
-void InitUninstControls() {
-
+int InitUninstControls() {
     progressbar = pMain->FindDescendent(StrToID((UCString)L"progress"));
     Nxt = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"next"));
     Bck = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"back"));
+    useless = (RichText*)pMain->FindDescendent(StrToID((UCString)L"useless"));
+    waitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"WaitAnimation"));
+    restartWaitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartWaitAnimation"));
+    progressmeter = (RichText*)pMain->FindDescendent(StrToID((UCString)L"R11Progress"));
+    Countdown = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartCountdown"));
+
+    if (!progressbar || !Nxt || !Bck || !useless || !waitAnimation || !restartWaitAnimation || !progressmeter || !Countdown) {
+        MainLogger.WriteLine(L"One or more required uninstaller controls are missing.", -35);
+        return -35;
+    }
 
     Nxt->AddListener(new EventListener(NavNext));
     Bck->AddListener(new EventListener(NavBack));
 
-    useless = (RichText*)pMain->FindDescendent(StrToID((UCString)L"useless"));
-
-    waitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"WaitAnimation"));
-    restartWaitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartWaitAnimation"));
-
-    progressmeter = (RichText*)pMain->FindDescendent(StrToID((UCString)L"R11Progress"));
-    Countdown = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartCountdown"));
-
     void (*fArr[])(Element*, Event*) = { HandleThemesChk, HandleAsdfChk, HandleWinverChk, HandleExplorerChk, HandleIconChk };
-    TouchCheckBox* tch;
-    std::wstring ws;
     for (int i = 0; i < 5; i++) {
-        ws = L"chk1" + std::to_wstring(i + 1);
-        tch = (TouchCheckBox*)pMain->FindDescendent(StrToID((UCString)ws.c_str()));
+        const std::wstring ws = L"chk1" + std::to_wstring(i + 1);
+        auto* tch = (TouchCheckBox*)pMain->FindDescendent(StrToID((UCString)ws.c_str()));
+        if (!tch) {
+            MainLogger.WriteLine(L"Required uninstaller checkbox " + ws + L" was not found.", -36);
+            return -36;
+        }
         tch->AddListener(new EventListener(fArr[i]));
     }
+
+    return S_OK;
 }
 
 int InitUninstaller() {
     if (!CheckVer(21343)) {
         err2 = -21343;
-        TaskDialog(NULL, NULL, L"no", L"stop", L"unsupported on this windows version", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
+        TaskDialog(NULL, NULL, L"Rectify12", L"Unsupported Windows version", L"Windows 10 build 21343 or newer is required.", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
         MainLogger.WriteLine(L"This Windows Build is not supported. Windows 10 Build 21343 and above is required.", err2);
         return err2;
     }
@@ -99,11 +104,16 @@ int InitUninstaller() {
     MainLogger.WriteLine(L"Initializing pages...\n\n\n");
     err2 = InitUninstPages();
     if (FAILED(err2)) {
-        err2 = -33;
         MainLogger.WriteLine(L"One or more pages failed to initialize correctly.\n", err2);
         return err2;
     }
-    InitUninstControls();
+
+    err2 = InitUninstControls();
+    if (FAILED(err2)) {
+        MainLogger.WriteLine(L"One or more uninstaller controls failed to initialize correctly.\n", err2);
+        return err2;
+    }
+
     useless->SetContentString((UCString)L"Uninstalling Rectify12");
     err2 = ChangeSheet();
     if (FAILED(err2)) {
