ca130e7e6d1384539a5fe513a3efde5a648dce35
RetroFrost <routerifter@gmail.com>
2026-08-21 20:05:30 +0100

    Add central Rectify12 product identity

---
+++ Rectify11Installer/ProductInfo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Rectify12 {
+    inline constexpr wchar_t ProductName[] = L"Rectify12";
+    inline constexpr wchar_t ProductVersion[] = L"0.1.0-dev";
+    inline constexpr wchar_t Publisher[] = L"RetroFrost";
+    inline constexpr wchar_t InstallFolder[] = L"Rectify12";
+    inline constexpr wchar_t InstalledExecutable[] = L"Rectify12Installer.exe";
+    inline constexpr wchar_t UninstallRegistryPath[] = L"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Rectify12";
+}
