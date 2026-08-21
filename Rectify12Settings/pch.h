f345053fd3b016114f88f1b3425d7fa314be4da7
RetroFrost <routerifter@gmail.com>
2026-08-21 21:57:20 +0100

    Include WinRT collections projection for Settings navigation

--- Rectify12Settings/pch.h
+++ Rectify12Settings/pch.h
@@ -7,6 +7,7 @@
 
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.System.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
