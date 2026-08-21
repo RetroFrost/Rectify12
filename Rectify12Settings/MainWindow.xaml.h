caf50b11819126380fd62bbead9fcf346be0b059
RetroFrost <routerifter@gmail.com>
2026-08-21 21:44:32 +0100

    Add Settings action handlers

--- Rectify12Settings/MainWindow.xaml.h
+++ Rectify12Settings/MainWindow.xaml.h
@@ -16,9 +16,30 @@ namespace winrt::Rectify12Settings::implementation
         void OpenWindowsSettings_Click(
             Windows::Foundation::IInspectable const& sender,
             Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void RestartExplorer_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void SaveExclusions_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void CreateRestoreSnapshot_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void ExportSettings_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void ImportSettings_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void RunHealthCheck_Click(
+            Windows::Foundation::IInspectable const& sender,
+            Microsoft::UI::Xaml::RoutedEventArgs const& args);
 
     private:
         void SetPageText(std::wstring_view title, std::wstring_view description);
+        void SetActionResult(bool success, std::wstring_view message);
+        void UpdateActionVisibility(Rectify12::Settings::Page page);
+        void LoadExclusionsIntoEditor();
     };
 }
 
