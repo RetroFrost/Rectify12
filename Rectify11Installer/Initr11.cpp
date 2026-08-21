34c0ef7f60bcce1f54bc5f7637944842e193aa27
RetroFrost <routerifter@gmail.com>
2026-08-21 23:04:31 +0100

    Harden installer DirectUI initialisation

--- Rectify11Installer/Initr11.cpp
+++ Rectify11Installer/Initr11.cpp
@@ -11,44 +11,35 @@ using namespace DirectUI;
 HRESULT err = 0;
 
 bool CheckVer(int build) {
-    OSVERSIONINFOEXA vInfo;
+    OSVERSIONINFOEXA vInfo{};
     vInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXA);
-    vInfo.dwBuildNumber =  build;
+    vInfo.dwBuildNumber = build;
     DWORDLONG dwlConditionMask = 0;
-    int op = VER_GREATER_EQUAL;
+    const int op = VER_GREATER_EQUAL;
     VER_SET_CONDITION(dwlConditionMask, VER_BUILDNUMBER, op);
-    if (!VerifyVersionInfoA(&vInfo, VER_BUILDNUMBER, dwlConditionMask)) return false;
-    return true;
+    return VerifyVersionInfoA(&vInfo, VER_BUILDNUMBER, dwlConditionMask) != FALSE;
 }
-
 
 bool GetUserAppMode() {
-    int i = 0;
-    LPCWSTR path = L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
-    HKEY hKey;
-    DWORD lResult = RegOpenKeyEx(HKEY_CURRENT_USER, path, 0, KEY_READ, &hKey);
-    if (lResult == ERROR_SUCCESS)
-    {
-        DWORD dwSize = NULL;
-        lResult = RegGetValue(hKey, NULL, L"AppsUseLightTheme", RRF_RT_DWORD, NULL, NULL, &dwSize);
-        if (lResult == ERROR_SUCCESS && dwSize != NULL)
-        {
-            DWORD* dwValue = (DWORD*)malloc(dwSize);
-            lResult = RegGetValue(hKey, NULL, L"AppsUseLightTheme", RRF_RT_DWORD, NULL, dwValue, &dwSize);
-            i = *dwValue;
-            free(dwValue);
-        }
-        RegCloseKey(hKey);
-    }
-    if (i == 1) return true;
-    return false;
-}
+    DWORD value = 0;
+    DWORD size = sizeof(value);
+    const LSTATUS result = RegGetValueW(
+        HKEY_CURRENT_USER,
+        L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+        L"AppsUseLightTheme",
+        RRF_RT_REG_DWORD,
+        nullptr,
+        &value,
+        &size);
 
+    // Preserve the historical Rectify behaviour if the preference is unavailable:
+    // dark mode is the safer visual default for the current installer resources.
+    return result == ERROR_SUCCESS && value != 0;
+}
 
 int InitPages() {
     for (int i = 1; i < MAXPAGE; i++) {
-
-        Element* page;
+        Element* page = nullptr;
         MainLogger.WriteLine(L"Setting XML parser from resource: " + to_wstring(IDR_UIFILE1 + i));
         err = pParser->SetXMLFromResource(IDR_UIFILE1 + i, hinst, hinst);
         if (FAILED(err)) {
@@ -67,14 +58,16 @@ int InitPages() {
 
         MainLogger.WriteLine(L"Creating PageMain Element inside " + n);
         err = pParser->CreateElement((UCString)L"PageMain", pContainer, NULL, NULL, &page);
-        if (FAILED(err)) {
+        if (FAILED(err) || !page) {
+            if (SUCCEEDED(err)) err = -33;
             MainLogger.WriteLine(L"Cannot create PageMain Element inside " + n, err);
+            return err;
         }
 
         std::wstring anim = L"animator" + std::to_wstring(i);
         MainLogger.WriteLine(L"Finding Element with ID: " + anim);
         Element* pAnimator = pMain->FindDescendent(StrToID((UCString)anim.c_str()));
-        if (FAILED(err)) {
+        if (!pAnimator) {
             err = -31;
             MainLogger.WriteLine(L"Element with ID: " + anim + L" not found", err);
             return err;
@@ -91,57 +84,55 @@ int InitPages() {
         MainLogger.WriteLine(L"Adding page " + to_wstring(i) + L" to page list");
         animArr.push_back(pAnimator);
         pageArr.push_back(pContainer);
-
         MainLogger.WriteLine(L"\n");
     }
     MainLogger.WriteLine(L"InitPages() completed", err);
     return err;
 }
 
-
-void InitControls() {
-
+int InitControls() {
     progressbar = pMain->FindDescendent(StrToID((UCString)L"progress"));
     Nxt = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"next"));
     Bck = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"back"));
-
-    Nxt->AddListener(new EventListener(NavNext));
-    Bck->AddListener(new EventListener(NavBack));
-
     browsebtn = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"browsebtn"));
 
     TouchButton* SYS = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"PatchSystem"));
     TouchButton* ISO = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"PatchISO"));
-
-    SYS->AddListener(new EventListener(NavSYS));
-    ISO->AddListener(new EventListener(NavISO));
-
     waitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"WaitAnimation"));
     restartWaitAnimation = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartWaitAnimation"));
-
     TouchButton* credits = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"credits"));
-
     TouchButton* Full = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"Full"));
     TouchButton* None = (TouchButton*)pMain->FindDescendent(StrToID((UCString)L"None"));
-
     progressmeter = (RichText*)pMain->FindDescendent(StrToID((UCString)L"R11Progress"));
     Countdown = (RichText*)pMain->FindDescendent(StrToID((UCString)L"RestartCountdown"));
 
-    void (*fArr[])(Element*, Event*) = { HandleThemesChk, HandleAsdfChk, HandleWinverChk, HandleExplorerChk};
-    TouchCheckBox* tch;
-    std::wstring ws;
+    if (!progressbar || !Nxt || !Bck || !browsebtn || !SYS || !ISO || !waitAnimation ||
+        !restartWaitAnimation || !credits || !Full || !None || !progressmeter || !Countdown) {
+        MainLogger.WriteLine(L"One or more required installer controls are missing.", -35);
+        return -35;
+    }
+
+    Nxt->AddListener(new EventListener(NavNext));
+    Bck->AddListener(new EventListener(NavBack));
+    SYS->AddListener(new EventListener(NavSYS));
+    ISO->AddListener(new EventListener(NavISO));
+
+    void (*fArr[])(Element*, Event*) = { HandleThemesChk, HandleAsdfChk, HandleWinverChk, HandleExplorerChk };
     for (int i = 0; i < 4; i++) {
-        ws = L"chk" + std::to_wstring(i + 1);
-        tch = (TouchCheckBox*)pMain->FindDescendent(StrToID((UCString)ws.c_str()));
+        const std::wstring ws = L"chk" + std::to_wstring(i + 1);
+        auto* tch = (TouchCheckBox*)pMain->FindDescendent(StrToID((UCString)ws.c_str()));
+        if (!tch) {
+            MainLogger.WriteLine(L"Required checkbox " + ws + L" was not found.", -36);
+            return -36;
+        }
         tch->AddListener(new EventListener(fArr[i]));
     }
 
     Full->AddListener(new EventListener(NavFull));
     None->AddListener(new EventListener(NavNone));
-
     credits->AddListener(new EventListener(OpenCredits));
+    return S_OK;
 }
-
 
 int ChangeSheet() {
     MainLogger.WriteLine(L"Setting parser object to resource: " + to_wstring(IDR_UIFILE1));
@@ -165,7 +156,8 @@ int ChangeSheet() {
         InstallFlags[L"LIGHTTHEME"] = false;
         InstallFlags[L"DARKTHEME"] = true;
     }
-    if (FAILED(err)) {
+    if (FAILED(err) || !v) {
+        if (SUCCEEDED(err)) err = -34;
         MainLogger.WriteLine(L"Failed to get Stylesheet information.", err);
         return err;
     }
@@ -189,26 +181,30 @@ int ChangeSheet() {
 int InitInstaller() {
     if (!CheckVer(21343)) {
         err = -21343;
-        TaskDialog(NULL, NULL, L"no", L"stop", L"unsupported on this windows version", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
+        TaskDialog(NULL, NULL, L"Rectify12", L"Unsupported Windows version", L"Windows 10 build 21343 or newer is required.", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
         MainLogger.WriteLine(L"This Windows Build is not supported. Windows 10 Build 21343 and above is required.", err);
         return err;
     }
-    if (!InternetCheckConnection(L"https://8.8.8.8/", FLAG_ICC_FORCE_CONNECTION, 0))
-    {
+    if (!InternetCheckConnection(L"https://www.microsoft.com/", FLAG_ICC_FORCE_CONNECTION, 0)) {
         err = -69;
-        TaskDialog(NULL, NULL, L"no", L"stop", L"internet needed", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
-        MainLogger.WriteLine(L"Rectify11 required active internet connection to be installed.", err);
+        TaskDialog(NULL, NULL, L"Rectify12", L"Internet connection required", L"Some installation components require an active internet connection.", TDCBF_OK_BUTTON, TD_ERROR_ICON, NULL);
+        MainLogger.WriteLine(L"Rectify12 requires an active internet connection for online installation components.", err);
         return err;
     }
 
     MainLogger.WriteLine(L"Initializing pages...\n\n\n");
     err = InitPages();
     if (FAILED(err)) {
-        err = -33;
         MainLogger.WriteLine(L"One or more pages failed to initialize correctly.\n", err);
         return err;
     }
-    InitControls();
+
+    err = InitControls();
+    if (FAILED(err)) {
+        MainLogger.WriteLine(L"One or more controls failed to initialize correctly.\n", err);
+        return err;
+    }
+
     err = ChangeSheet();
     if (FAILED(err)) {
         MainLogger.WriteLine(L"Failed to change stylesheet.", err);
@@ -216,4 +212,3 @@ int InitInstaller() {
     }
     return err;
 }
-
