5ac0782da101caee7878f4b53510d2e52d3f8b87
RetroFrost <routerifter@gmail.com>
2026-08-21 21:05:20 +0100

    Include protocol activation APIs in Rectify12 Settings

--- Rectify12Settings/pch.h
+++ Rectify12Settings/pch.h
@@ -5,6 +5,7 @@
 #include <restrictederrorinfo.h>
 #include <hstring.h>
 
+#include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.System.h>
 #include <winrt/Microsoft.UI.Xaml.h>
